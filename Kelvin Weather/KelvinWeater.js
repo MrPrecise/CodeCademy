de0bcec0ea55118a38491ace9e4b583fb15968e5
Eirik <MrPrecise@users.noreply.github.com>
2022-10-08 20:42:43 +0200

    Create KelvinWeater.js

---
+++ Kelvin Weather/KelvinWeater.js
@@ -0,0 +1,10 @@
+const kelvin = 300;
+const celsius = kelvin - 273;
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
+
+
+
+newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log("The temperature is " + fahrenheit + " degrees Fahrenheit.")
+console.log(newton);
